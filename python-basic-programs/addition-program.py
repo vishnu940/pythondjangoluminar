2e2126b6c0c02592327c86835ace097cfc490b67
vishnu <vishnuvscms@gmail.com>
2021-01-23 18:09:45 +0530

    Adding python basic programs

---
+++ python-basic-programs/addition-program.py
@@ -0,0 +1,5 @@
+#program to add two numbers
+num1=int(input("Enter 1st no:"))
+num2=int(input("Enter 2nd no:"))
+num3=num1+num2
+print("Addition=",num3)
---
+++ python-basic-programs/length-program.py
@@ -0,0 +1,3 @@
+#program to find the length of a string
+name=input("Enter the name")
+print("length is:",len(name))
